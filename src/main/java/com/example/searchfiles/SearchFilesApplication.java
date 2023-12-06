3737935f2cfa13e0cb339400afe7f811afc295ae
RassvetalovMaksim <rassvetalovpavel@gmail.com>
2023-12-06 22:15:27 +0500

    Initial commit

---
+++ src/main/java/com/example/searchfiles/SearchFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searchfiles/SearchFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
